f8ddd40f0cbcf2feb83ed9b8b5bda0edfb8ec643
Phan Văn Tiến <86212960+tienpvse1@users.noreply.github.com>
2023-05-24 16:13:46 +0700

    add eslint

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -6,23 +6,21 @@ local formatting = null_ls.builtins.formatting
 local lint = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 local diagnostics = null_ls.builtins.diagnostics
-local filetypes = {"typescript", "javascript", "go", "typescriptreact","javascriptreact"}
 local sources = {
-   formatting.prettier,
-   formatting.stylua,
-   formatting.gofmt,
-   diagnostics.misspell.with({filetypes}),
-   code_actions.gomodifytags,
-   code_actions.impl,
-   code_actions.refactoring,
-   diagnostics.tsc,
-   lint.shellcheck,
-   require("typescript.extensions.null-ls.code-actions"),
-
+  formatting.prettier,
+  formatting.stylua,
+  formatting.gofmt,
+  code_actions.gomodifytags,
+  code_actions.impl,
+  code_actions.refactoring,
+  code_actions.eslint,
+  diagnostics.tsc,
+  lint.shellcheck,
+  lint.eslint,
+  require "typescript.extensions.null-ls.code-actions",
 }
 
 null_ls.setup {
-   debug = true,
-   sources = sources,
+  debug = true,
+  sources = sources,
 }
-
